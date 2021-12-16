f17d20e5694a5c6d776f2b1590e621ef666ec31d
hygkui <hygkui@gmail.com>
2021-12-16 20:02:19 +0800

    add axios get cmc data

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,14 +1,13 @@
-import { App, Configuration } from '@midwayjs/decorator';
-import { ILifeCycle } from '@midwayjs/core';
-import * as orm from '@midwayjs/orm';
-import { Application } from 'egg';
-import { join } from 'path';
+import { App, Configuration } from "@midwayjs/decorator";
+import { ILifeCycle } from "@midwayjs/core";
+import * as orm from "@midwayjs/orm";
+import { Application } from "egg";
+import { join } from "path";
+import * as axios from "@midwayjs/axios";
 
 @Configuration({
-  imports: [
-    orm
-  ],
-  importConfigs: [join(__dirname, './config')],
+  imports: [orm, axios],
+  importConfigs: [join(__dirname, "./config")],
   conflictCheck: true,
 })
 export class ContainerLifeCycle implements ILifeCycle {
